963f7e9cdb7b3766244d2d59264b9c3b6d89d649
vadim <va9im.shat0v@gmail.com>
2024-10-30 19:47:57 +0300

    lsp

--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -1,9 +1,10 @@
 local on_attach = function(_, bufnr)
-  local opts = { noremap=true, silent=true, buffer=bufnr }
+  local opts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
   vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
   vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+  vim.keymap.set("n", "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", { desc = "Switch Source/Header (C/C++)", buffer = bufnr })
 end
 
 return {
@@ -11,11 +12,39 @@ return {
   dependencies = { "neovim/nvim-lspconfig" },
   config = function()
     require("mason-lspconfig").setup {
-      ensure_installed = { "clangd" } 
+      ensure_installed = { "clangd" }
     }
     require("lspconfig").clangd.setup {
-        on_attach = on_attach,
+      on_attach = on_attach,
+      root_dir = function(fname)
+        return require("lspconfig.util").root_pattern(
+          "Makefile",
+          "configure.ac",
+          "configure.in",
+          "config.h.in",
+          "meson.build",
+          "meson_options.txt",
+          "build.ninja"
+        )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(fname)
+          or require("lspconfig.util").find_git_ancestor(fname)
+      end,
+      capabilities = {
+        offsetEncoding = { "utf-16" },
+      },
+      cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+        "--completion-style=detailed",
+        "--function-arg-placeholders",
+        "--fallback-style=llvm",
+      },
+      init_options = {
+        usePlaceholders = true,
+        completeUnimported = true,
+        clangdFileStatus = true,
+      },
     }
   end
 }
-
